25eaa2807dc29bda10735cdfcca104c1f8d7d87b
Rafael Della Giustina <rafael.della64@gmail.com>
2024-09-20 16:52:50 -0300

    commit 36:50

--- awesome_notes/lib/main.dart
+++ awesome_notes/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notes/constants.dart';
 import 'package:awesome_notes/main_page.dart';
 import 'package:flutter/material.dart';
 
@@ -14,8 +15,19 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Awesome Notes',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 248, 214, 19)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 248, 214, 19)),
         useMaterial3: true,
+        fontFamily: 'Poppins',
+        scaffoldBackgroundColor: background,
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              backgroundColor: Colors.transparent,
+              titleTextStyle: TextStyle(
+                  color: primary,
+                  fontSize: 32,
+                  fontFamily: 'Fredoka',
+                  fontWeight: FontWeight.bold),
+            ),
       ),
       home: const MainPage(),
     );
--- awesome_notes/lib/main_page.dart
+++ awesome_notes/lib/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notes/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,7 +10,6 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-
   final List<String> dropDownOptions = ['Date modified', 'Date created'];
 
   late String dropDownValue = dropDownOptions.first;
@@ -21,76 +21,97 @@ class _MainPageState extends State<MainPage> {
         title: const Text("Awesome Notes 📝"),
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const FaIcon(FontAwesomeIcons.rightFromBracket))
+            onPressed: () {},
+            icon: const FaIcon(FontAwesomeIcons.rightFromBracket),
+            style: IconButton.styleFrom(
+                backgroundColor: primary,
+                foregroundColor: white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(
+                      color: black,
+                    ))),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton.large(onPressed: (){}, child: FaIcon(FontAwesomeIcons.plus),),
+      floatingActionButton: FloatingActionButton.large(
+        onPressed: () {},
+        child: FaIcon(FontAwesomeIcons.plus),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
-                decoration: const InputDecoration(
-              labelText: "Search Notes...",
-              prefixIcon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+              decoration: InputDecoration(
+                  hintText: "Search Notes...",
+                  prefixIcon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+                  fillColor: white,
+                  filled: true,
+                  isDense: true,
+                  contentPadding: EdgeInsets.zero,
+                  prefixIconConstraints:
+                      BoxConstraints(minWidth: 42, minHeight: 42),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: primary),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: primary),
+                  )),
             ),
-            ),
-
             Row(
               children: [
-                IconButton(onPressed: () {}, 
-                icon: FaIcon(FontAwesomeIcons.arrowDown)),
+                IconButton(
+                    onPressed: () {}, icon: FaIcon(FontAwesomeIcons.arrowDown)),
                 DropdownButton<String>(
                   value: dropDownValue,
                   items: dropDownOptions
-                  .map(
-                    (e) => DropdownMenuItem(
-                    value: e,
-                    child: Text(e),
-                    ),
-                    )
-                    .toList(), 
-                    onChanged: (newValue){
-                      setState(() {
-                        dropDownValue = newValue!;
-                      });
-                    },
-              ),
-              Spacer(),
-              IconButton(
-                onPressed:(){}, 
-                icon: FaIcon(FontAwesomeIcons.bars)
-              ),
-            ],
+                      .map(
+                        (e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      dropDownValue = newValue!;
+                    });
+                  },
+                ),
+                Spacer(),
+                IconButton(
+                    onPressed: () {}, icon: FaIcon(FontAwesomeIcons.bars)),
+              ],
             ),
             Expanded(
               child: GridView.builder(
                 itemCount: 8,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, 
-                  crossAxisSpacing: 4, 
-                  mainAxisSpacing: 4
-                ), 
-                itemBuilder: (context, int index){ return Container(
-                  child: Column(
-                    children: [
-                      Text("This is going to be a title"),
-                      Row(
-                        children: [
-                          Container(
-                            child: Text("First chip"),
+                    crossAxisCount: 2, crossAxisSpacing: 4, mainAxisSpacing: 4),
+                itemBuilder: (context, int index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        Text("This is going to be a title"),
+                        Row(
+                          children: [
+                            Container(
+                              child: Text("First chip"),
                             ),
                           ],
                         ),
                         Text("Content"),
-                        Row(children: [
-                          Text("09 Aug, 2027"),
-                          FaIcon(FontAwesomeIcons.trash)
-                        ],
-                      ),
-                    ],
-                  ),
+                        Row(
+                          children: [
+                            Text("09 Aug, 2027"),
+                            FaIcon(FontAwesomeIcons.trash)
+                          ],
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),
